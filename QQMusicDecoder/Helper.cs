2cc761545eefef44075c6300a90199f5b2d03de1
XY Wang <wxy11787@foxmail.com>
2023-04-04 21:11:49 +0800

    Code cleanup

--- QQMusicDecoder/Helper.cs
+++ QQMusicDecoder/Helper.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Specialized;
-using System.Net;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json;
 using System.Xml;
 
 namespace QQMusicDecoder
